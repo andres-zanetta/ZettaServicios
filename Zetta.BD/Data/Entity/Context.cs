effb5fceb4341822e042b26b00622ed8847c99ff
Andrés Zane <andresnicolaszanetta@gmail.com>
2024-07-08 12:59:27 -0300

    Agregar archivos de proyecto.

---
+++ Zetta.BD/Data/Entity/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Zetta.BD.Data.Entity;
+
+namespace Zetta.BD.Data.Entity
+{
+    public class Context : DbContext
+    {
+        public DbSet<Presupuesto> Presupuestos { get; set; }
+
+    }
+}
---
+++ Zetta.BD/Data/Entity/Presupuestar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Zetta.BD.Data.Entity
+{
+    [Index(nameof())]
+    public class Presupuesto:EntityBase
+    {
+    }
+}
